458be056a15753c173977ce413545146c0999041
ReinerJasin <62686723+ReinerJasin@users.noreply.github.com>
2026-01-27 11:13:24 +0800

    prediction simply by calling a function

--- project/utils/utils.py
+++ project/utils/utils.py
@@ -35,15 +35,7 @@ def euclidean_distance(vec1, vec2, axis=None):
     return np.linalg.norm(vec1 - vec2, axis=axis)
 
 
-def recognize_face(test_projection, train_projections, labels):
-    """
-    Check the euclidean distance to each projections and find the projection that is closest to the current image.
-    
-    Args:
-    test_projection: Description
-    train_projections: Derived from (face_diff @ eigenfaces)
-    labels: Description
-    """
+def compute_min_distance(test_projection, train_projections, labels):
     distances = euclidean_distance(train_projections, test_projection, axis=1)
     print(f'distances: {distances}')
     
@@ -52,8 +44,44 @@ def recognize_face(test_projection, train_projections, labels):
     
     return labels[min_index], distances[min_index]
 
+def predict_face(
+    image_path,
+    mean_faces,
+    eigenfaces,
+    train_projections,
+    label_list,
+    TARGET_WIDTH,
+    TARGET_HEIGHT,
+    threshold=None,
+    display_plot=False
+    ):
+
+    test_face = preprocess_face(image_path, TARGET_WIDTH=TARGET_WIDTH, TARGET_HEIGHT=TARGET_HEIGHT, display_plot=display_plot)
+    test_projection = project_face(test_face, mean_faces, eigenfaces)
+
+    predicted_label, distance = compute_min_distance(
+        test_projection,
+        train_projections,
+        label_list
+    )
+
+    print("\nW/O threshold:")
+    print("Predicted label:", predicted_label)
+    print("Distance:", distance)
+
+    print(f"\nWith threshold: {threshold}")
+    if threshold is not None and distance > threshold:
+        print("Unknown face")
+    else:
+        print("Recognized as:", predicted_label)
+
+
+
+
 
 def recognize_face_centroid(test_projection, class_centroids):
+    
+    
     distances = {
         label: euclidean_distance(test_projection, centroid)
         for label, centroid in class_centroids.items()
@@ -69,17 +97,25 @@ def compute_distances_to_centroids(test_projection, class_centroids):
         distances[label] = euclidean_distance(test_projection, centroid)
     return distances
 
-def predict_label_from_distances(distances):
+def predict_label_from_distances(distances, label_mapping=None, threshold=None):
     predicted_label = min(distances, key=distances.get)
     min_distance = distances[predicted_label]
 
+    if threshold is not None and min_distance > threshold:
+            return "Unknown", min_distance
+    
+    if label_mapping is not None:
+        predicted_label = next(
+            name for name, idx in label_mapping.items()
+            if idx == predicted_label
+        )
+    
     return predicted_label, min_distance
 
-def recognize_from_distances(distances, use_threshold=True, threshold=5000):
+def recognize_from_distances(distances, threshold=None):
     label, distance = predict_label_from_distances(distances)
 
-    if use_threshold:
-        if distance > threshold:
+    if threshold is not None and distance > threshold:
             return "Unknown", distance
 
     return label, distance
@@ -89,7 +125,7 @@ def recognize_face_eigenface(
     mean_faces,
     eigenfaces,
     class_centroids,
-    threshold
+    threshold=None
 ):
     # Step 1: Project face
     test_projection = project_face(test_face, mean_faces, eigenfaces)
@@ -112,7 +148,7 @@ def recognize_face_from_image(
     class_centroids,
     TARGET_WIDTH,
     TARGET_HEIGHT,
-    threshold
+    threshold=None
 ):
 
     # Step 1: Preprocess image
@@ -135,10 +171,62 @@ def recognize_face_from_image(
 # Utility function for prediction
 from pathlib import Path
 
-def predict_label_from_distances(distances):
-    predicted_label = min(distances, key=distances.get)
-    min_distance = distances[predicted_label]
-    return predicted_label, min_distance
+def predict_face_with_centroids(
+    image_path,
+    mean_faces,
+    eigenfaces,
+    class_centroids,
+    TARGET_WIDTH,
+    TARGET_HEIGHT,
+    label_mapping=None,
+    threshold=None,
+    display_plot=False
+):
+
+    test_face = preprocess_face(image_path, TARGET_WIDTH=TARGET_WIDTH, TARGET_HEIGHT=TARGET_HEIGHT, display_plot=display_plot)
+    test_projection = project_face(test_face, mean_faces, eigenfaces)
+
+    distances = compute_distances_to_centroids(
+        test_projection,
+        class_centroids
+    )
+    
+    predicted_label, min_distance = predict_label_from_distances(distances, label_mapping=label_mapping)
+    
+    labels = list(distances.keys())
+    values = list(distances.values())
+    labels, values = zip(*sorted(zip(labels, values), key=lambda x: x[1]))
+    
+    image = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (TARGET_WIDTH, TARGET_HEIGHT))
+    
+    plt.subplot(2, 1, 1)
+    plt.imshow(image, cmap="gray")
+    plt.title(f"Pred: {predicted_label}\nDist: {min_distance:.1f}")
+    plt.axis("off")
+    
+    plt.subplot(2, 1, 2)
+    plt.bar(labels, values, color="gray")
+    
+    if threshold is not None:
+        plt.axhline(y=threshold, color='red', linestyle='--', linewidth=0.5, label='Threshold')
+        if values[0] < threshold:
+            plt.bar(labels[0], values[0])
+        # else:
+        #     plt.bar(labels[0], values[0], color="blue")
+    else:
+        plt.bar(labels[0], values[0])
+            
+    if label_mapping is not None:
+        # For x-ticks
+        x = np.arange(len(labels))
+        names = [k for k, v in label_mapping.items() if v in labels]
+
+        plt.xticks(x, names, rotation=45, ha="right") 
+    else:
+        plt.xticks(rotation=45, ha="right")
+    plt.ylabel("Distance")
+    plt.title("Distance to Centroids")
 
 def predict_batch_label_display(
     test_image_list,
@@ -147,6 +235,7 @@ def predict_batch_label_display(
     class_centroids,
     TARGET_WIDTH,
     TARGET_HEIGHT,
+    label_mapping=None,
     input_image_dir = Path('input_images')
     ):
     
@@ -170,7 +259,7 @@ def predict_batch_label_display(
             class_centroids
         )
 
-        predicted_label, min_distance = predict_label_from_distances(distances)
+        predicted_label, min_distance = predict_label_from_distances(distances, label_mapping=label_mapping)
 
         # load image again for display
         image = cv2.imread(str(test_image_path), cv2.IMREAD_GRAYSCALE)
@@ -189,6 +278,8 @@ def predict_batch_detailed_label_display(
     class_centroids,
     TARGET_WIDTH,
     TARGET_HEIGHT,
+    threshold=None,
+    label_mapping=None,
     input_image_dir = Path('input_images')
     ):
     
@@ -211,7 +302,7 @@ def predict_batch_detailed_label_display(
             class_centroids
         )
 
-        predicted_label, min_distance = predict_label_from_distances(distances)
+        predicted_label, min_distance = predict_label_from_distances(distances, threshold=threshold, label_mapping=label_mapping)
 
         labels = list(distances.keys())
         values = list(distances.values())
@@ -231,9 +322,25 @@ def predict_batch_detailed_label_display(
         # Plot Bar Chart
         bar_pos = 1 * cols + idx + 1
         plt.subplot(rows, cols, bar_pos)
-        plt.bar(labels, values)
-        plt.bar(labels[0], values[0], color="red")
-        plt.xticks(rotation=45, ha="right")
+        plt.bar(labels, values, color="gray")
+        if threshold is not None:
+            plt.axhline(y=threshold, color='red', linestyle='--', linewidth=0.5, label='Threshold')
+            if values[0] < threshold:
+                plt.bar(labels[0], values[0])
+            # else:
+            #     plt.bar(labels[0], values[0], color="blue")
+        else:
+            plt.bar(labels[0], values[0])
+            
+        if label_mapping is not None:
+            # For x-ticks
+            x = np.arange(len(labels))
+            names = [k for k, v in label_mapping.items() if v in labels]
+
+            plt.xticks(x, names, rotation=45, ha="right") 
+        else:
+            plt.xticks(rotation=45, ha="right")
+        
         plt.ylabel("Distance")
         plt.title("Distance to Centroids")
 
